2024a593d82188d4b28354e394b731600ebd8402
somang-lim <somangjuha@gmail.com>
2022-06-28 15:59:26 +0900

    add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+function BlogPage() {
+ return (
+  <Layout pageTitle="imhope posts">
+   <p>My coll posts will go in here</p>
+  </Layout>
+ );
+}
+
+export default BlogPage;

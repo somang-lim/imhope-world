62415110f18872c9685e6d09152d0f818e24e14d
somang-lim <somangjuha@gmail.com>
2022-06-28 10:25:29 +0900

    add TestPage

---
+++ src/pages/test-space.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TestSpacePage() {
+    return (
+        <main>
+            <title>test-space</title>
+            <h1>test-space 페이지입니다.</h1>
+        </main>
+    );
+}
+
+export default TestSpacePage;
---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TestPage() {
+    return (
+        <main>
+            <title>test</title>
+            <h1>test 페이지입니다.</h1>
+        </main>
+    );
+}
+
+export default TestPage;
